be541f1c55a138703efcf63c2bbc2bc188954f4c
Clemana <ananaclem@gmail.com>
2025-04-16 14:11:52 +0200

    exercice

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = "Ianis"
+age = 29
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+age = age + 10
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,23 @@
-# Ecrivez votre code ici !
+nombre1 = input("Entrez le premier nombre : ")
+nombre2 = input("Entrez le deuxième nombre : ")
+if not (nombre1.isnumeric() and nombre2.isnumeric()):
+    raise SystemExit("Fin du programme")
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+operation = input("Entrez une opération (+, -, * ou /) : ")
+
+if operation not in ['+', '-', '*', '/']:
+    raise SystemExit("Fin du programme : opération invalide.")
+
+if operation == '+':
+    resultat = nombre1 + nombre2
+elif operation == '-':
+    resultat = nombre1 - nombre2
+elif operation == '*':
+    resultat = nombre1 * nombre2
+elif operation == '/':
+    if nombre2 == 0:
+        raise SystemExit("Fin du programme : division par zéro impossible.")
+    resultat = round(nombre1 / nombre2, 2)  
+
+print("Résultat :", resultat)
