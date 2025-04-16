b55f7f64b269234591b2fc7522dd0aa309683944
Clemana <ananaclem@gmail.com>
2025-04-16 14:34:17 +0200

    exercice partie 2

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,18 @@
-# Ecrivez votre code ici !
+nombres = input("Entrez une liste de nombres  séparés par des virgules :")
+liste = nombres.split(",")
+liste_entiers = []
+for element in liste:
+    liste_entiers.append(int(element))
+
+somme = sum(liste_entiers)
+print("La somme est :", somme)
+
+moyenne = somme / len(liste_entiers)
+print("La moyenne est :", moyenne)
+
+compteur = 0
+for nombre in liste_entiers:
+    if nombre > moyenne:
+        compteur += 1
+
+print("Il y a", compteur, "nombres plus grands que la moyenne.")
