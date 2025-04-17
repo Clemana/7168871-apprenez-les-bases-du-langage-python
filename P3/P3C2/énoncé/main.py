0d3ee0defeed8860f3bca9c783f337cf6f35ff6f
Clemana <ananaclem@gmail.com>
2025-04-17 11:55:25 +0200

    correction

--- P3/P3C2/énoncé/main.py
+++ P3/P3C2/énoncé/main.py
@@ -2,5 +2,7 @@ from bs4 import BeautifulSoup
 with open("index.html", 'r') as file:
      soup = BeautifulSoup(file, 'html.parser')
 
+title = soup.title.string
+print("Titre de la page :", title)
 
 
