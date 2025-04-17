f21f11a26040973d9570ccffc875571c15673a1c
Clemana <ananaclem@gmail.com>
2025-04-17 10:37:57 +0200

    exercice P3C1

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,5 @@
-# Ecrivez votre code ici !
+import operations 
+resultat_addition = operations.addition(3,5)
+resultat_multiplication = operations.multiplication(8,2)
+print(resultat_addition,resultat_multiplication)
+
